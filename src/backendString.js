50ee609f09a9624adc5a0f545ba72be0eef8ea73
Abhishek Verma <abhishekverma@Abhisheks-MacBook-Pro-4.local>
2024-12-13 00:52:16 +0530

    fix

--- src/backendString.js
+++ src/backendString.js
@@ -1,4 +1,4 @@
-const backend = "https://carblogappbackend-1.onrender.com/"
+const backend = "https://carblogappbackend-1.onrender.com"
 // const backend = "http://localhost:3000"
 
 export default backend;
