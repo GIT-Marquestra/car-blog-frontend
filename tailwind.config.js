84a6ee461b238d0db00195d4273722296158a53e
Abhishek Verma <abhishekverma@Abhisheks-MacBook-Pro-4.local>
2024-12-12 18:56:30 +0530

    intial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import { defineConfig } from 'tailwindcss';
-export default defineConfig( {
+export default {
     content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
@@ -46,4 +45,4 @@ export default defineConfig( {
       ],
     },
   
-  })
+  }
