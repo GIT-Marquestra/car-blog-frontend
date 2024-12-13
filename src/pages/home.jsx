e46e075d20ae34e5f5a72c0c49a62fc63ee1ec99
Abhishek Verma <abhishekverma@Abhisheks-MacBook-Pro-4.local>
2024-12-13 16:24:47 +0530

    fix

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -492,15 +492,9 @@ const Contact = () => {
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-              <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
+              <li><a href="https://www.instagram.com/ig_abhii.verma/">Instagram</a></li>
+              <li><a href="https://www.linkedin.com/feed/?trk=guest_homepage-basic_google-one-tap-submit">LinkedIn</a></li>
+              <li><a href="https://github.com/GIT-Marquestra">Github</a></li>
             </ul>
           </div>
           <a className="btn btn-ghost text-xl">Contact </a>
